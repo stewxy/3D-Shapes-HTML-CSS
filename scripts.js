e72c198be8f9f6402a2d8c0422de762c3c6fef37
Stephen <stewxy13@gmail.com>
2023-03-03 16:39:53 +1300

    removed top reflection and messed around with animations

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-// JavaScript source code
+/*let div = document.querySelector(".cube");
+let i = 90; //degrees to rotate
+
+const rotateIt = () => {
+    div.style.transform = "rotate(" + i + "deg)";
+    i = i + 90;
+}
+div.addEventListener("mouseover", rotateIt);*/
